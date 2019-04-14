f81f6c7a9255f1c40bfa032e3223522689d9261d
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-04-14 20:15:47 +0300

    update composer + add tests

---
+++ tests/src/AbstractTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ByTIC\GouttePhantomJs\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTest
+ * @package ByTIC\GouttePhantomJs\Tests
+ */
+abstract class AbstractTest extends TestCase
+{
+}
---
+++ tests/src/ClientFactoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ByTIC\GouttePhantomJs\Tests;
+
+use ByTIC\GouttePhantomJs\Clients\ClientFactory;
+use ByTIC\GouttePhantomJs\Clients\PhantomJs\ClientBridge;
+use Goutte\Client;
+
+/**
+ * Class ClientFactoryTest
+ * @package ByTIC\GouttePhantomJs\Tests
+ */
+class ClientFactoryTest extends AbstractTest
+{
+    public function testGetGenericClient()
+    {
+        $client = ClientFactory::getGenericClient();
+
+        self::assertInstanceOf(Client::class, $client);
+        self::assertInstanceOf(ClientBridge::class, $client->getClient());
+    }
+}
